74a72aa934302af837b6afa13314c2a25ac357df
osmanghani1435 <osmanghani.a143@gmail.com>
2024-07-06 03:28:34 -0700

    osman

---
+++ go practice/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 10
+	t := 100
+	L := (a * t) / 2
+	fmt.Println(L)
+
+	helloworld := "hello" + " " + "world"
+	fmt.Println(helloworld)
+
+	osmanghani := "osman" + " " + "ghani"
+	fmt.Println(osmanghani)
+
+}
